840e3ccb0ff7e027b89980bc3f25e81429985eb0
Jonathan Alexander <jonathandalexander@gmail.com>
2016-08-15 14:25:08 -0400

    start building profileContainer

---
+++ frontend_react/src/components/ProfileContainer.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+
+const ProfileContainer = class extends Component {
+  constructor(props){
+    super(props)
+    this.state = {edit: false}
+  }
+
+  render(){
+    <div>Hello</div>
+  }
+}
+
+export default ProfileContainer
+//Header Container
+///user's name
+///user's company
+///user's location
+///user's organization
+///user's image
