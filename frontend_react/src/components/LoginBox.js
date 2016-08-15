0576ce1e4a8b7d274b6fa2f591e654eb5e9d836b
Dan Gorman <danielhgorman@gmail.com>
2016-08-15 14:19:27 -0400

    redux form prototype

--- frontend_react/src/components/LoginBox.js
+++ frontend_react/src/components/LoginBox.js
@@ -1,43 +1,39 @@
 import React, { Component, PropTypes } from 'react'
 import {createStore, combineReducers} from 'redux';
 import {reducer as formReducer} from 'redux-form';
+import { reduxForm } from 'redux-form'
 export const fields = [ 'firstName', 'lastName', 'email', 'sex', 'favoriteColor', 'employed', 'notes' ]
 
-const reducers = {
-  // ... your other reducers here ...
-  form: formReducer     // <---- Mounted at 'form'. See note below.
-}
-const reducer = combineReducers(reducers);
-const store = createStore(reducer);
-
-import {reduxForm} from 'redux-form';
+//
 
 class ContactForm extends Component {
+
+  handleFormSubmit(){
+  }
+
   render() {
     const {fields: {firstName, lastName, email}, handleSubmit} = this.props;
     return (
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
         <div>
           <label>First Name</label>
-          <input type="text" placeholder="First Name" {...firstName}/>
+          <input type="text" placeholder="First Name" {...firstName} />
         </div>
         <div>
           <label>Last Name</label>
-          <input type="text" placeholder="Last Name" {...lastName}/>
+          <input type="text" placeholder="Last Name" {...lastName} />
         </div>
         <div>
           <label>Email</label>
           <input type="email" placeholder="Email" {...email}/>
         </div>
-        <button type="submit">Submit</button>
+        <input type="submit" value="Submit" />
       </form>
     );
   }
 }
 
-ContactForm = reduxForm({ // <----- THIS IS THE IMPORTANT PART!
-  form: 'contact',                           // a unique name for this form
-  fields: ['firstName', 'lastName', 'email'] // all the fields in your form
-})(ContactForm);
-
-export default ContactForm;
+export default reduxForm({
+  form: 'contact',
+  fields: ['firstName', 'lastName', 'email']
+}, null, {formReducer})(ContactForm);
--- frontend_react/src/store/configureStore.js
+++ frontend_react/src/store/configureStore.js
@@ -1,11 +1,16 @@
 import { createStore, compose, applyMiddleware } from 'redux'
 import ReduxPromise from 'redux-promise'
+import rootReducer from '../reducers/index'
+
+
 
 export default function configureStore(initialState) {
     const store = createStore(
+ 				rootReducer,
         initialState,
          compose (
      applyMiddleware(ReduxPromise),
+
      window.devToolsExtension ? window.devToolsExtension() : f => f
    )
     )
