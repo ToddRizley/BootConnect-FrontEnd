9dee5b84d57b986482815393f7f1cbbf3bb67215
Dan Gorman <danielhgorman@gmail.com>
2016-08-16 12:50:40 -0400

    post generates a new user

--- frontend_react/src/actions/addUser.js
+++ frontend_react/src/actions/addUser.js
@@ -1,21 +1,16 @@
 import $ from 'jquery'
 
 export default function addUser(props) {
-
   const URL = 'http://localhost:3000/api/v1/users'
-  // const request = axios.post(url)
-  // console.log('request', request)
-  debugger
-  const request = $.ajax({
+  const response = $.ajax({
     url:URL,
     type:"POST",
     data: JSON.stringify({user: props}),
     contentType:"application/json; charset=utf-8",
     dataType:"json"
   })
-  
+
   return {
     type: 'ADD_USER',
-    payload: request
-  }
-}
+    payload: response
+  }}
