30e5aa001f8e9a5e0732c595f8112526d665f0b0
ToddRizley <todd.rizley@gmail.com>
2016-08-16 16:51:49 -0400

    update welcome page

---
+++ frontend_react/src/components/welcomePage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+const welcomePage = class extends Component {
+
+
+  render(){
+    return(
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default welcomePage
