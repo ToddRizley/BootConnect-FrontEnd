3ee86aeacec3e3bc6d6f9ee859812d0ec4a4a38e
Jonathan Alexander <jonathandalexander@gmail.com>
2016-08-17 12:30:09 -0400

    add user bio and exp forms

--- frontend_react/src/components/UserExperienceForm.js
+++ frontend_react/src/components/UserExperienceForm.js
@@ -5,7 +5,7 @@ import addUser from '../actions/addUser'
 
 
 
-class UserBioForm extends Component {
+class UserExperienceForm extends Component {
 
   constructor(props) {
     super(props)
@@ -19,9 +19,7 @@ class UserBioForm extends Component {
   }
 
   handleFormSubmit(props) {
-    this.setState({
-      enabled: !this.state.enabled
-    })
+    this.setState({enabled: false})
     this.props.addUser(props).then( ()=>{
       var router = require('react-router')
       router.browserHistory.push('/profile')
@@ -30,16 +28,16 @@ class UserBioForm extends Component {
   }
 
   render() {
-    const {fields: {bio}, handleSubmit} = this.props;
-    var bioInput = this.state.enabled ? <input type="textarea" placeholder="Well, when I was a girl/boy/larva" {...bio} /> : <input disabled="disabled" type="textarea" placeholder="Well, when I was a girl/boy/larva" {...bio} />
+    const {fields: {experience}, handleSubmit} = this.props;
+    var expInput = this.state.enabled ? <input type="textarea" placeholder="First things first..." {...experience} /> : <input disabled="disabled" type="textarea" placeholder="First things first..." {...experience} />
     var submitButton = this.state.enabled ? <input type="submit" value="Submit" /> : <div/>
 
     return (
       <div className="bio">
       <button onClick={this.toggleState.bind(this)}> Edit </button>
       <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-          <label>Tell us about your life! </label>
-          { bioInput }
+          <label> Share your experiences! </label>
+          { expInput }
           { submitButton }
       </form>
       </div>
@@ -53,6 +51,6 @@ function mapStateToProps(state) {
   }
 
 export default reduxForm({
-  form: 'userBio',
-  fields: ['bio']
-}, null, { addUser })(UserBioForm);
+  form: 'userExperience',
+  fields: ['experience']
+}, null, { addUser })(UserExperienceForm);
