453aeb9aed7d3171e00c1f6c656af86decab8bdc
Jonathan Alexander <jonathandalexander@gmail.com>
2016-08-24 17:06:22 -0400

    styling changes

--- frontend_react/src/components/ProfileContainer.js
+++ frontend_react/src/components/ProfileContainer.js
@@ -11,12 +11,14 @@ import JobList from './JobList'
 import ArticleForm from './ArticleForm'
 import ArticleList from './ArticleList'
 import SearchUsersContainer from './SearchUsers'
+import NavbarContainer from './NavbarContainer'
 
 const Profile = class extends Component {
 
   render(){
     return(
       <div>
+        <NavbarContainer currentUser={this.props.currentUser} />
         <HeaderContainer currentUser={this.props.currentUser} />
         <br/><br/>
         <UserBioContainer currentUser={this.props.currentUser} />
