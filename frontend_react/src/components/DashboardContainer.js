917462ef8758a9b860949e272fc0f104ddab10b8
ToddRizley <todd.rizley@gmail.com>
2016-08-25 10:07:23 -0400

    add routes to navbar, add my profile link on navbar, build out frame container for Dashboard

--- frontend_react/src/components/DashboardContainer.js
+++ frontend_react/src/components/DashboardContainer.js
@@ -23,7 +23,25 @@ import {Button} from 'react-bootstrap'
 const Dashboard = class extends Component {
   render(){
     return(
-      <div/>
+      <div className="dashboard-wrapper">
+      <Row>
+        <Col>
+          <NavbarContainer currentUser={this.props.currentUser} />
+        </Col>
+      </Row>
+      <Row>
+       <Col md={4} >
+        <SearchJobsContainer currentUser={this.props.currentUser} />
+       </Col>
+       <Col md={4}>
+        <ArticleList currentUser={this.props.currentUser}/>
+       </Col>
+       <Col md={4}>
+        <JobList currentUser={this.props.currentUser}/>
+       </Col>
+      </Row>
+
+      </div>
     )
   }
 }
