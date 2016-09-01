af76ed2ddee112e452a588e29bb56582a7c87fdf
Jonathan Alexander <jonathandalexander@gmail.com>
2016-09-01 11:09:32 -0400

    clean up article form

--- frontend_react/src/actions/removeArticle.js
+++ frontend_react/src/actions/removeArticle.js
@@ -1,12 +1,9 @@
 import axios from 'axios'
 
 function removeArticle(){
-
   let id = this.props.id
   const url = `http://localhost:3000/api/v1/articles/${id}`
-
   const request = axios.post(url)
-
 
   return {
     type: 'REMOVE_ARTICLE',
