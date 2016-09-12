4b747de02d96ea8cc6998212c2ceeff735b33db8
ToddRizley <todd.rizley@gmail.com>
2016-09-12 15:56:06 -0400

    small fixes

--- frontend_react/src/reducers/locationReducer.js
+++ frontend_react/src/reducers/locationReducer.js
@@ -1,7 +1,7 @@
 export default function locationReducer(state = {locationInfo: ""}, action) {
   switch(action.type){
     case 'FETCH_LAT_LONG':
-      return Object.assign({}, state,  {locationInfo: action.payload});
+      return {locationInfo: action.payload};
     default:
       return state
   }
