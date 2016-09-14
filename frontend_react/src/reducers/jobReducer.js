80cd98604a7bc3e03cb80d41641bff17232d323b
ToddRizley <todd.rizley@gmail.com>
2016-09-14 10:17:10 -0400

    create dumb jobs container, get addjobs to render on submission

--- frontend_react/src/reducers/jobReducer.js
+++ frontend_react/src/reducers/jobReducer.js
@@ -1,5 +1,7 @@
 export default function jobReducer(state = {jobList: []}, action){
   switch(action.type){
+    case 'ADD_JOB':
+      return Object.assign({}, state,  {jobList: action.payload.data});
     case 'FETCH_JOBS':
       return Object.assign({}, state, { jobList: action.payload.data });
     case 'FETCH_FILTERED_JOBS':
